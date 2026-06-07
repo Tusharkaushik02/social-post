fadd21818c5c4b2fb1dbbb8b4cd82bd414e3bd62
Tushar kaushik <tusharkaushik520@gmail.com>
2026-06-07 20:15:11 +0530

    update schema of user and post

--- backend/src/model/post.model.js
+++ backend/src/model/post.model.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
+    User: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     image: {
         type: String,
         required: true
--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true }
+    passwordHash: { type: String, required: true },
+    displayname: { type: String, default: '' },
+    avatarUrl: { type: String, default: '' },
+    bio: { type: String, default: '' }
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
